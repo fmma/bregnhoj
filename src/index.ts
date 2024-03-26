6e54b1f732d74117f136d8a009dbd69b24e0ca38
Frederik meisner <frederik.meisner@gmail.com>
2024-03-26 10:43:40 +0100

    lisogkarsten site

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,12 @@ import type { Bapp } from './components/App';
 import './components/Sites';
 
 const params =new URLSearchParams(window.location.search)
-const site = params.get('site');
+const siteFromParams = params.get('site');
 
-
+const siteNameFromHtml = document.getElementById('thescript')?.getAttribute('data-site'); 
 const app: Bapp = document.createElement('b-app') as Bapp;
-app.siteRoot = site ?? "bregnhoj";
+const site = siteFromParams ?? siteNameFromHtml ?? "bregnhoj";
+
+app.siteRoot = site;
 app.dev = true;
 document.body.appendChild(app)
