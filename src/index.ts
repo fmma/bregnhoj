58e7b22f2b6a2cf0213ec87befc392fd39a91f78
Frederik meisner <frederik.meisner@gmail.com>
2025-05-21 14:39:10 +0200

    Use VITE_SITE_ROOT for deployment instead of switch on base url.

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,7 @@ import './components/App';
 import './components/TextEditorControls'
 import type { Bapp } from './components/App';
 
-let site = "snesltest";
-
-switch (import.meta.env.BASE_URL as string) {
-    case 'https://bregnhoj.com':
-        site = 'bregnhoj';
-        break;
-    case 'https://meisnermadsen.dk':
-        site = 'lisogkarsten';
-        break;
-    case 'https://snesl.dk/bregnhoj':
-        site = 'snesltest';
-        break;
-}
-
+let site = import.meta.env.VITE_SITE_ROOT ?? "snesltest";
 const app: Bapp = document.createElement('b-app') as Bapp;
-
-
 app.site_root = site;
 document.body.appendChild(app);
