4cea5a3a02ca85def1599d1eb401ed24fc82fb6d
frma <frederik.meisner@gmail.com>
2025-05-21 20:18:22 +0200

    Use .env files for different sites

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 
 import './components/App';
 import './components/TextEditorControls'
-import type { Bapp } from './components/App';
 
-let site = import.meta.env.VITE_SITE_ROOT ?? "snesltest";
-const app: Bapp = document.createElement('b-app') as Bapp;
-app.site_root = site;
-document.body.appendChild(app);
+
